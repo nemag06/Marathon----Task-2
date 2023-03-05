8b45777346c4fa504ebecf3468de441159beadb3
Magomet Bekov <magafif57@gmail.com>
2023-03-05 17:29:03 +0400

    Initial Commit

---
+++ Marathon -- Task 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marathon -- Task 2
+//
+//  Created by Magomet Bekov on 05.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
